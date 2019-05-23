b0a51e6e3bfc319ddf14241fbe70d750d4d03c4a
cindychea <cindy_chea@hotmail.com>
2019-05-23 14:47:21 -0400

    added an about page to first web app

--- my_first_web_app/urls.py
+++ my_first_web_app/urls.py
@@ -23,13 +23,25 @@ def home_page(request):
     return HttpResponse(response)
 
 def portfolio_page(request):
-    random_number = randint(0,100)
-    image_url = 'https://picsum.photos/400/600/?image={}'.format(random_number)
-    context = {'gallery_image': image_url}
+    image_urls = []
+    for i in range(5):
+        random_number = randint(0,100)
+        image_urls.append('https://picsum.photos/400/600/?image={}'.format(random_number))
+    context = {'gallery_images': image_urls}
     response = render(request, 'gallery.html', context)
     return HttpResponse(response)
+
+def about_page(request):
+    context = {
+        'skills': ['dancing', 'climbing', 'asking random questions'],
+        'interests': ['health and wellness', 'people and relationships', 'family dynamics']
+    }
+    response = render(request, 'about.html', context)
+    return HttpResponse(response)
+
 
 urlpatterns = [
     path('home/', home_page),
     path('portfolio/', portfolio_page),
+    path('about/', about_page),
 ]
